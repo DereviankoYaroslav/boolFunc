65349beaf759e5fcb8abe35a5f9615e5e470e614
DereviankoYaroslav <yarik0009258@gmail.com>
2021-06-01 19:59:05 +0300

    5commit

--- main.c
+++ main.c
@@ -14,6 +14,8 @@ int *binaryElements(int *func, int size, int count);
 
 int *anfRepresentation(int *func, int size);
 
+int *functionElems(int *arr, int *arr2, int size, int count);
+
 
 int main(int args, char **argv) {
     SetConsoleOutputCP(1251);
@@ -23,7 +25,7 @@ int main(int args, char **argv) {
     int size = raiseToPower(2, n);
     int *binElems = elemsForN(size);
     int f[] = {1, 0, 0, 1, 0, 1, 1, 0};
-    int sizeOfF = sizeof(f)/sizeof(int);
+    int sizeOfF = sizeof(f) / sizeof(int);
 
     for (int i = 0; i < size; ++i) {
         printf("%d ", binElems[i]);
@@ -38,7 +40,7 @@ int main(int args, char **argv) {
         printf("\n");
     }
 
-    int * func2 = anfRepresentation(f, sizeOfF);
+    int *func2 = anfRepresentation(f, sizeOfF);
     for (int i = 0; i < sizeOfF; ++i) {
         printf("%d ", func2[i]);
     }
@@ -53,6 +55,14 @@ int main(int args, char **argv) {
         printf("\n");
     }
 
+    int *ar3 = functionElems(func2, ar, size, n);
+    for (int i = 0; i < n; ++i) {
+        printf("x%d = ", i + 1);
+        for (int j = 0; j < size; ++j) {
+            printf("%d ", ar3[i * size + j]);
+        }
+        printf("\n");
+    }
 
     free(func2);
 
@@ -60,12 +70,37 @@ int main(int args, char **argv) {
 }
 
 int *binaryElements(int *arr, int size, int count) {
-    int *result = calloc(size*count, sizeof(int));
+    int *result = calloc(size * count, sizeof(int));
     for (int i = 0; i < size; ++i) {
         int *bin = valueToBinary(arr[i], count);
         for (int j = 0, k = count - 1; j < count; ++j, k--) {
             result[j * size + i] = bin[k];
             //printf("%d\n",bin[j]);
+        }
+    }
+    return result;
+}
+
+int *functionElems(int *arr, int *arr2, int size, int count) {
+    int *result = calloc(size * count, sizeof(int));
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] >= 0) {
+            int *bin = valueToBinary(arr[i], count);
+            for (int j = 0, k = count - 1; j < count; ++j, k--) {
+                if (bin[j] != 0) {
+                    printf("nnn %d\n",j*size+i);
+                    int calc = arr2[arr[i]*size];
+                    //printf("%d\n",calc);
+                    //printf("%d\n",bin[j]);
+                } else{
+                    printf("nnn %d\n",j*size+i);
+                }
+                result[j * size + i] = bin[k];
+            }
+        } else {
+            for (int j = 0, k = count - 1; j < count; ++j, k--) {
+                result[j * size + i] = -99;
+            }
         }
     }
     return result;
@@ -88,7 +123,7 @@ int raiseToPower(int num, int pow) {
 }
 
 int *elemsForN(int size) {
-    int *result = calloc(size,sizeof(int));
+    int *result = calloc(size, sizeof(int));
     for (int i = 0; i < size; ++i) {
         result[i] = i;
     }
@@ -97,15 +132,14 @@ int *elemsForN(int size) {
 
 int *anfRepresentation(int *func, int sizeOfF) {
     int newSize = sizeOfF;
-    int *result = calloc(newSize, sizeof (int));
+    int *result = calloc(newSize, sizeof(int));
     int j;
     for (int i = 0; i < newSize; ++i) {
         if (func[i] != 0) {
             result[i] = i;
             ++j;
-        }
-        else if (func[i] == 0) {
-            result[i] = 0;
+        } else if (func[i] == 0) {
+            result[i] = -1;
         }
     }
     printf("\n");
